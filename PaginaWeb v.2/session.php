7684487ecad8d0229fbdbd9ea3bef984edb3aea2
AdrianSaut <adisautean@gmail.com>
2021-05-30 15:30:53 +0200

    PAginaWeb

---
+++ PaginaWeb v.2/session.php
@@ -0,0 +1,13 @@
+<?php
+$con = mysqli_connect("localhost", "root", "", "escobarsnow3");
+session_start();
+$user_check=$_SESSION['login_user'];
+$ses_sql=mysqli_query($con,"select DNI,id_client from client where DNI='$user_check'");
+$row=mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+$loggedin_session=$row['DNI'];
+$loggedin_id=$row['id_client'];
+if(!isset($loggedin_session) || $loggedin_session==NULL) {
+	echo "Go back";
+	header("Location: index.php");
+}
+?>
